25173e000af4c089ba4983fe8a280df5d6f370d9
Ged0oo <mohmmednagy@gmail.com>
2025-11-16 12:38:07 +0200

    add problem 4

--- 04_ProblemSolving/Sheets/sheet2/Main.java
+++ 04_ProblemSolving/Sheets/sheet2/Main.java
@@ -68,6 +68,20 @@ public class Main {
         return head;
     }
 
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return head;
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (temp.next != null && temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            }
+            else temp = temp.next;
+        }
+
+        return head;
+    }
+    
     public static void main(String[] args) {
 
     }
