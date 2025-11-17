34e9267d92ee66a3286da2b4038720c88eed4433
Ged0oo <mohmmednagy@gmail.com>
2025-11-17 09:20:57 +0200

    add problem 8

--- 04_ProblemSolving/Sheets/sheet2/Main.java
+++ 04_ProblemSolving/Sheets/sheet2/Main.java
@@ -109,6 +109,18 @@ public class Main {
 
         return ret;
     }
+    
+    public int searchInsert(int[] nums, int target) {
+        int right = nums.length - 1, left = 0, mid;
+        while(left <= right) {
+            mid = left + (right - left)/2;
+            if(nums[mid] == target) return mid;
+            else if(target > nums[mid]) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+    
     public static void main(String[] args) {
 
     }
