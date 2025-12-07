50e4443b3b9e4f9c659ddb1ed9f7cd702579fcd5
Ged0oo <mohmmednagy@gmail.com>
2025-12-07 21:29:21 +0200

    add quadratics package

---
+++ 06_JavaCore/Labs/Lab3.2/Main.java
@@ -0,0 +1,9 @@
+import quadratics.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Parameters p = new Parameters(1, -3, 2);
+        Roots r = QuadraticEquation.solve(p);
+        System.out.println("Roots: " + r.root1() + ", " + r.root2());
+    }
+}
