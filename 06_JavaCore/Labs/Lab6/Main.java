4bc288805952a236e661d0719022057bea221a54
Ged0oo <mohmmednagy@gmail.com>
2025-12-10 11:47:11 +0200

    add lab6 - java

---
+++ 06_JavaCore/Labs/Lab6/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args){
+        MyDictionary dict = new MyDictionary();
+        dict.addWord("world");
+        dict.addWord("Hello");
+        dict.addWord("Nagy");
+        dict.addWord("Mohamed");
+        dict.addWord("MoSalah");
+        dict.addWord("Yasmeen");
+        dict.addWord("Mariam");
+        dict.addWord("word");
+        dict.addWord("Salah");
+        dict.addWord("Morsy");
+        dict.addWord("Mazen");
+        dict.addWord("Messi");
+        dict.addWord("Apple");
+        dict.addWord("Samsung");
+        dict.addWord("Gemy");
+        dict.addWord("Ged0oo");
+        dict.addWord("Zoo");
+        
+        // dict.printDict();
+        dict.printCharList('a');
+    }
+}
---
+++ 06_JavaCore/Labs/Lab6/MyDictionary.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class MyDictionary {
+
+    TreeMap<Character, TreeSet<String>> dict;
+
+    public MyDictionary() {
+        dict = new TreeMap<>();
+    }
+
+
+    public void addWord(String word) {
+        if (word == null || word.isEmpty()) return;
+        char key = word.toUpperCase().charAt(0);
+        TreeSet<String> wordsSet = dict.getOrDefault(key, new TreeSet<>());
+        wordsSet.add(word);
+        dict.put(key, wordsSet);
+    }
+
+
+    public void printDict(){
+        for(Character ch:dict.keySet()) printCharList(ch);
+    }
+
+
+    public void printCharList(char ch) {
+        char key = Character.toUpperCase(ch);
+        Set<String> set = dict.get(key);
+
+        if (set == null || set.isEmpty()) {
+            System.out.println("No words found starting with '" + key + "'.");
+            return;
+        }
+
+        System.out.println("\n--- Words starting with '" + key + "' ---");
+        for (String word : set) System.out.println(word);
+        System.out.println("------------------------------------");
+    }
+}
